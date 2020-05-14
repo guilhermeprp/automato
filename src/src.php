78220a1f90dff46a301c6cd11e4d8968b68da9ac
guilhermeprp <guipimenta.224@gmail.com>
2020-05-14 09:49:05 -0300

    hotfix src

--- src/src.php
+++ src/src.php
@@ -1,17 +1,20 @@
 <?php
 
 echo "Digite seu email: ";
-
 $email = trim(fgets(STDIN, 1024));
 
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+echo "Digite seu CPF: ";
+$entrada2 = trim(fgets(STDIN, 1024));
+
+echo "Digite a placa do seu veículo: ";
+$placa = trim(fgets(STDIN, 1024));
+
+if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i", $email)) {
   echo ("Email válido\n");
 } else {
   echo ("Email inválido\n");
 }
 
-echo "Digite seu CPF: ";
-$entrada2 = trim(fgets(STDIN, 1024));
 $cpf = $entrada2;
 $cpf = str_replace(".", "", $cpf);
 $cpf = str_replace("-", "", $cpf);
@@ -22,10 +25,8 @@ if (preg_match('/^[0-9]{11}$/', $cpf)) {
   echo ("CPF inválido\n");
 }
 
-echo "Digite a placa do seu veículo: ";
-$placa = trim(fgets(STDIN, 1024));
 
-if (preg_match('/[A-Za-z]{3}-[0-9]{4}/', $placa)) { 
+if (preg_match('/^[A-Za-z]{3}-[0-9]{4}$/', $placa)) { 
   echo ("Placa válida\n");
 } else {  
   echo ("Placa inválida\n");};
